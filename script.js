5b986f6e51bd6c2f96c453d9ffd1e1edd9b4ccf6
sterringproduct-stack <sterringproduct@gmail.com>
2026-07-07 19:01:44 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,23 @@
 const liffId = "2010567384-7VDFmpAy";
 
 async function main() {
+  try {
     await liff.init({ liffId });
 
     if (!liff.isLoggedIn()) {
-        liff.login();
-        return;
+      liff.login();
+      return;
     }
 
     const profile = await liff.getProfile();
 
     document.getElementById("profile").innerHTML =
-        profile.displayName;
+      "สวัสดี " + profile.displayName;
+
+  } catch (err) {
+    document.getElementById("profile").innerHTML =
+      "Error : " + err;
+  }
 }
 
 main();
